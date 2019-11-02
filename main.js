6a68f8c5257868f713ef9264d8c35bd4307a19d4
goldlure <goldlure@gmail.com>
2019-11-02 14:13:28 +0200

    position canvas set to center, canvas move to html, delete buttons, control set to keyboard, add width and height for Boat

--- main.js
+++ main.js
@@ -1,154 +1,209 @@
+// INIT
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 var myBoat;
+var myBoatWidth = 230;
+var myBoatHeight = 140;
 var myPlane;
 var myParashutist;
 var mySea;
 var myScore;
 var myLives;
-var score = 0;
-var lives = 3;
+var myExit;
+var score =0;
+var lives =3;
+var rightPressed = false;
+var leftPressed = false;
+
+// KEYBOARD
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+function keyDownHandler(e) {
+    if ("code" in e) {
+        switch (e.code) {
+            case "Unidentified":
+                break;
+            case "ArrowRight":
+            case "Right": // IE <= 9 and FF <= 36
+            case "KeyD":
+                rightPressed = true;
+                return;
+            case "ArrowLeft":
+            case "Left": // IE <= 9 and FF <= 36
+            case "KeyA":
+                leftPressed = true;
+                return;
+            case "Escape":
+                myGameArea.stop();
+                return;
+            case "Enter":
+                myGameArea.start();
+                return;
+            default:
+                return;
+        }
+    }
+
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+    else if (e.keyCode == 27) {
+        myGameArea.stop();
+    }
+    else if (e.keyCode == 13) {
+        myGameArea.start();
+    }
+}
+function keyUpHandler(e) {
+    if ("code" in e) {
+        switch (e.code) {
+            case "Unidentified":
+                break;
+            case "ArrowRight":
+            case "Right": // IE <= 9 and FF <= 36
+            case "KeyD":
+                rightPressed = false;
+                return;
+            case "ArrowLeft":
+            case "Left": // IE <= 9 and FF <= 36
+            case "KeyA":
+                leftPressed = false;
+                return;
+            default:
+                return;
+        }
+    }
+
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 
 
 function startGame() {
-	mySea = new component(0, 30, 0, 500, "resources/sea.png", "image");
-	myBoat = new component(30, 30, 500, 400, "resources/boat.png", "image");
-	myPlane = new component(30, 30, 1000, 20, "resources/plane.png", "image");
-	myParashutist = new component(
-		30,
-		30,
-		1000,
-		20,
-		"resources/parachutist.png",
-		"image"
-	);
-	myScore = new textComponent("20px", "Consolas", 20, 40, "SCORE: ");
-	myLives = new textComponent("20px", "Consolas", 20, 70, "LIVES: ");
-	myGameArea.start();
+    mySea = new component(0, 30, 0, 500, "resources/sea.png", "image");
+    myBoat = new component(myBoatWidth, myBoatHeight, 500, 400, "resources/boat.png", "image");
+    myPlane = new component(30, 30, 1000, 20, "resources/plane.png", "image");
+    myParashutist = new component(
+        30,
+        30,
+        1000,
+        20,
+        "resources/parachutist.png",
+        "image"
+    );
+    myScore = new textComponent("20px", "Consolas", 20, 40);
+    myLives = new textComponent("20px", "Consolas", 20, 70);
+    myExit = new textComponent("20px", "Consolas", 20, canvas.height-20);
+    myGameArea.start();
 }
 
 var myGameArea = {
-	canvas: document.createElement("canvas"),
-	start: function() {
-		this.canvas.width = 1000;
-		this.canvas.height = 600;
-		this.canvas.style.background = "url(resources/background.png)";
-		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-		this.score = score;
-		this.interval = setInterval(updateGameArea, 20);
-	},
-	clear: function() {
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	},
-	stop: function() {
-		this.context.font = "20px Georgia";
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		this.context.fillText("END", 450, 300);
-		clearInterval(this.interval);
-	}
+    start: function () {
+        score = 0;
+        lives = 3;
+        rightPressed = false;
+        leftPressed = false;
+        this.score = score;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    },
+    stop: function () {
+        ctx.font = "20px Consolas";
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillText("END", canvas.width/2, canvas.height/2);
+        ctx.fillText("Press ENTER to start again", 20, canvas.height-20);
+        clearInterval(this.interval);
+    }
 };
 
-function textComponent(size, font, x, y, text) {
-	this.size = size;
-	this.font = font;
-	this.x = x;
-	this.y = y;
-	this.update = function(text, value) {
-		this.text = text + value;
-		ctx = myGameArea.context;
-		ctx.font = this.size + " " + this.font;
-		ctx.fillText(this.text, this.x, this.y);
+function textComponent(size, font, x, y) {
+    this.size = size;
+    this.font = font;
+    this.x = x;
+    this.y = y;
+    this.update = function (text, value) {
+        this.text = text + value;
+        ctx.font = this.size + " " + this.font;
+        ctx.fillText(this.text, this.x, this.y);
     };
 }
 
-function component(width, height, x, y, src, type) {
-	this.type = type;
-	this.width = width;
-	this.height = height;
-	this.speedX = 0;
-	this.speedY = 0;
-	this.x = x;
-	this.y = y;
-	const img = new Image();
-	this.update = function() {
-		ctx = myGameArea.context;
-		if (this.type == "text") {
-			ctx.font = this.width + " " + this.height;
-			ctx.fillText(this.text, this.x, this.y);
-		} else if (this.type == "image") {
-			img.src = src;
-			ctx.drawImage(img, this.x, this.y);
-		}
-	};
-	this.float = function() {
-		if (this.x <= 10) {
-			this.x = 11;
-		} else if (this.x >= 1001) {
-			this.x = 1000;
-		} else {
-			this.x += this.speedX;
-		}
-	};
-	this.fly = function() {
-		this.x--;
-		if (this.x <= -100) {
-			this.x = x;
-		}
-	};
-	this.drop = function() {
-		if (this.y > 400) {
-			
-			let boatPos = myBoat.getPos();
-			let parachutistPos = myParashutist.getPos();
-			if (parachutistPos < boatPos + 190 && parachutistPos >= boatPos) {
-				score+=10;
-			}
-			else {
-				lives--;
-				if (lives == 0){
-					myGameArea.stop();
-				}
-			}
-			this.y = y;
-			this.x = myPlane.getPos();
-		} else {
-			this.y++;
-		}
-	};
-	this.getPos = function() {
-		return this.x;
-	};
-	this.stop = function() {
-		this.x = x;
-	};
+function component(width, height, x, y, src) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    const img = new Image();
+    this.update = function () {
+        img.src = src;
+        ctx.drawImage(img, this.x, this.y);
+    };
+    this.float = function () {
+        if (rightPressed) {
+            this.x += 5;
+        }
+        else if (leftPressed) {
+            this.x -= 5;
+        }
+        if (this.x <= 0) {
+            this.x = 1;
+        } else if (this.x > canvas.width-this.width) {
+            this.x = canvas.width-this.width;
+        } else {
+            this.x += this.speedX;
+        }
+    };
+    this.fly = function () {
+        this.x--;
+        if (this.x <= -100) {
+            this.x = x;
+        }
+    };
+    this.drop = function () {
+        if (this.y > 400) {
+            let boatPos = myBoat.getPos();
+            let parachutistPos = myParashutist.getPos();
+            if (parachutistPos < boatPos + myBoatWidth && parachutistPos >= boatPos) {
+                score += 10;
+            }
+            else {
+                lives--;
+                if (lives == 0) {
+                    myGameArea.stop();
+                }
+            }
+            this.y = y;
+            this.x = myPlane.getPos();
+        } else {
+            this.y++;
+        }
+    };
+    this.getPos = function () {
+        return this.x;
+    };
 }
 
 function updateGameArea() {
-	myGameArea.clear();
-	myBoat.float();
-	myPlane.fly();
-
-	myParashutist.drop();
-	// mySea.flow();
-	mySea.update();
-	myBoat.update();
-	myPlane.update();
-	myParashutist.update();
-	myScore.update("SCORE: ", score);
-	myLives.update("LIVES: ", lives);
-}
-
-function moveleft() {
-	myBoat.speedX -= 2;
-}
-
-function moveright() {
-	myBoat.speedX += 2;
-}
-
-function clearmove() {
-	myBoat.speedX = 0;
-}
-
-function stop() {
-	myGameArea.stop();
+    myGameArea.clear();
+    myBoat.float();
+    myPlane.fly();
+    myParashutist.drop();
+    mySea.update();
+    myBoat.update();
+    myPlane.update();
+    myParashutist.update();
+    myScore.update("SCORE: ", score);
+    myLives.update("LIVES: ", lives);
+    myExit.update("Press ESC to quit", '');
 }
